638ab7ba6e976c6aacd8f71d1db2ee9de3766f17
VISHAL M <vishal300406@gmail.com>
2026-02-17 14:36:05 +0530

    Radix sort analysis

--- DAA/lab/sort/Radix_sort.c
+++ DAA/lab/sort/Radix_sort.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(int arr[], int n)
+{   
+    printf("\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 void count_sort(int arr[], int n, int pos)
 {
     if (n <= 0)
         return;
+    
+    int countArray[10] = {0};
+    int outputArray[n];
+    for (int i = 0; i < n; i++){
+        int digit = (arr[i] / pos) % 10;
+        countArray[digit]++;
+        
+    } 
+    
+    printf("1st loop Count array after sorting by position %d: ", pos);
+    print_array(countArray, 10);
 
-    int count[10];
-    for (int i = 0; i < 10; i++)
-        count[i] = 0;
-
-    for (int i = 0; i < n; i++)
-        count[(arr[i] / pos) % 10]++;
-
-    for (int i = 1; i < 10; i++){
-        count[i] += count[i - 1];
-    }     
-
-    int output[n];
-    for (int i = n - 1; i >= 0; i--)
-    {
-        output[count[(arr[i] / pos) % 10] - 1] = arr[i];
-        count[(arr[i] / pos) % 10]--;
+    for (int i = 1; i < 10; i++)
+        countArray[i] += countArray[i - 1];
+    
+    printf("2nd loop Count array after sorting by position %d: ", pos);
+    print_array(countArray, 10);
+    
+    for (int i = n - 1; i >= 0; i--){
+        int digit = (arr[i] / pos) % 10;
+        outputArray[countArray[digit] - 1] = arr[i];
+        countArray[digit]--;
     }
 
-    for (int i = 0; i < n; i++)
-        arr[i] = output[i];
+    for (int i = 0; i < n; i++){
+        arr[i] = outputArray[i];
+    }
+    
+    printf("Count array after sorting by position %d: ", pos);
+    print_array(countArray, 10);
+    printf("Output array after sorting by position %d: ", pos);
+    print_array(outputArray, n);
 }
 
 int getMax(int arr[], int n)
@@ -39,11 +58,10 @@ int getMax(int arr[], int n)
 
 void radix_sort(int arr[], int n)
 {
-    int max = getMax(arr, n);
-
-    for (int pos = 1; max / pos > 0; pos *= 10)
+    int max_value = getMax(arr, n);
+    for (int exp = 1; max_value / exp > 0; exp *= 10)
     {
-        count_sort(arr, n, pos);
+        count_sort(arr, n, exp);
     }
 }
 
@@ -55,6 +73,7 @@ int main()
     printf("Original array: \n");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
+    printf("\n");
     radix_sort(arr, n);
     printf("\nSorted array: \n");
     for (int i = 0; i < n; i++)
